3db2b244fab119eea71a80094324f875585785cd
satetheus <personal@cwmccullough.com>
2025-12-25 01:14:16 -0700

    hopefully now the gitignore is fixed?

--- armstrong_number_checker/one/target/debug/deps/one-1a55d2e5485c21c5.d
+++
@@ -1,5 +0,0 @@
-/home/chris/Documents/projects/one_hour_projects/armstrong_number_checker/one/target/debug/deps/libone-1a55d2e5485c21c5.rmeta: src/main.rs
-
-/home/chris/Documents/projects/one_hour_projects/armstrong_number_checker/one/target/debug/deps/one-1a55d2e5485c21c5.d: src/main.rs
-
-src/main.rs:
--- armstrong_number_checker/one/target/debug/deps/one-8ddb97b397ff5daa.d
+++
@@ -1,9 +0,0 @@
-/home/chris/Documents/projects/one_hour_projects/armstrong_number_checker/one/target/debug/deps/libone-8ddb97b397ff5daa.rmeta: src/main.rs Cargo.toml
-
-/home/chris/Documents/projects/one_hour_projects/armstrong_number_checker/one/target/debug/deps/one-8ddb97b397ff5daa.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
