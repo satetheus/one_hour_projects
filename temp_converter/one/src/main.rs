ae9d82a9128c593981a346918450389d202ab3db
satetheus <personal@cwmccullough.com>
2025-12-25 13:34:15 -0700

    added support for cmd parameters for temp converter one & added basic documentation

--- temp_converter/one/src/main.rs
+++ temp_converter/one/src/main.rs
@@ -1,7 +1,24 @@
+use std::env;
+
+/// when using this as a stand alone program, you must supply a single
+/// temperature number & measure character (f, c, or k) in that order.
+/// example:
+/// ```
+/// ./target/release/one 85 f
+/// ```
 fn main() {
-    todo!();
+    let args: Vec<_> = env::args().collect();
+    if args.len() == 3 {
+        let temp = args[1].parse::<f32>().expect("temp not a number");
+        let measure = args[2].chars().next().expect("couldn't find measure");
+        println!("{:?}", convert_temp(temp, measure));
+    } else {
+        println!("Please supply a temperature & a measure type (f, c, or k)");
+    }
 }
 
+/// convert_temp accepts a temp (f32) & a measure (char) and then returns
+/// an array of tuples of the temperature in all 3 measures (f, c, & k).
 fn convert_temp(temp: f32, measure: char) -> [(f32, char); 3] {
     match measure {
         'f' => [
